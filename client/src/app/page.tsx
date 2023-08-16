7ba33c3cd99ae6e3f5a81cba8c29a4ea97f5f944
PakkuDon <pakku.pd@gmail.com>
2023-08-17 00:29:58 +1000

    Fetch restaurants and display names on frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,29 @@
-export default function Home() {
+async function getRestaurants() {
+  const response = await fetch("http://localhost:8080/api/restaurants")
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data")
+  }
+  return response.json()
+}
+
+export default async function Home() {
+  const data = await getRestaurants()
+
   return (
     <main>
-      <h1>
-        Good Feeds
-      </h1>
+      <header>
+        <h1>
+          Good Feeds
+        </h1>
+      </header>
+      <main>
+        <ul>
+          {data.map((restaurant: any) => (
+            <li key={restaurant.id}>{restaurant.title}</li>
+          ))}
+        </ul>
+      </main>
     </main>
   )
 }
