b233afe1343957c6d107dbbbb6693a63aa981e7b
PakkuDon <pakku.pd@gmail.com>
2023-08-16 23:57:53 +1000

    Add table to store links to restaurants' websites

--- api/db/create_db.sql
+++ api/db/create_db.sql
@@ -10,3 +10,13 @@ CREATE TABLE IF NOT EXISTS restaurants (
 
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS restaurant_links (
+    id INT NOT NULL AUTO_INCREMENT,
+    url TEXT NOT NULL,
+    label TEXT NOT NULL,
+    restaurant_id INT NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(id)
+);
