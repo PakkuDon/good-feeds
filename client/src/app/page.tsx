ef66aa780b082a8a48763345338308f9dc672912
PakkuDon <pakku.pd@gmail.com>
2023-08-28 23:29:59 +1000

    Set up page layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,19 @@
-async function getRestaurants() {
+interface Restaurant {
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+}
+
+const Map = ({ locations }: { locations: Restaurant[] }) => (
+  <ul>
+    {locations.map((location: Restaurant) => (
+      <li key={location.id}>{location.name}</li>
+    ))}
+  </ul>
+)
+
+async function getRestaurants(): Promise<Restaurant[]> {
   const response = await fetch("http://localhost:8080/api/restaurants")
 
   if (!response.ok) {
@@ -11,18 +26,17 @@ export default async function Home() {
   const data = await getRestaurants()
 
   return (
-    <main>
-      <header>
+    <main className="main">
+      <header className="main-header">
         <h1>
           Good Feeds
         </h1>
       </header>
-      <main>
-        <ul>
-          {data.map((restaurant: any) => (
-            <li key={restaurant.id}>{restaurant.name}</li>
-          ))}
-        </ul>
+      <aside className="main-sidebar">
+        Sidebar
+      </aside>
+      <main className="main-content">
+        <Map locations={data} />
       </main>
     </main>
   )
