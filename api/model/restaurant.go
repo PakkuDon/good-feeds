c9467e25b8d7262d74251a56a01cc6dca1fe7ef5
PakkuDon <pakku.pd@gmail.com>
2024-06-10 00:20:22 +1000

    Remove references to restaurant.image_url in API
    This field is not populated or used client-side

--- api/model/restaurant.go
+++ api/model/restaurant.go
@@ -10,7 +10,6 @@ type Link struct {
 type Restaurant struct {
 	ID          int64     `json:"id"`
 	Name        string    `json:"name"`
-	ImageURL    string    `json:"imageUrl"`
 	Description string    `json:"description"`
 	Address     string    `json:"address"`
 	Latitude    float64   `json:"latitude"`
